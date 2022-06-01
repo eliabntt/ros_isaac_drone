be830f610bfa422540b6199cb75d4e4175aa48c5
Elia Bonetto <eliabntt94@gmail.com>
2022-06-01 12:04:31 +0200

    updating with time limit

--- bash_process.zsh
+++ bash_process.zsh
@@ -2,8 +2,8 @@
 echo "Output folder: $1"
 echo "Environment folder: $2"
 echo "Tmp out folder: $3"
-echo "Lower limit: $4"
-echo "Lower limit: $5"
+echo "Lower folder number: $4"
+echo "Offset: $5"
 echo "isaac folder: $6"
 source ~/.zshrc
 folder=$2/
@@ -33,21 +33,28 @@ do
 	rm ${3}/${currentenv}/ -rf
 	rm ${1}/${currentenv}/ -rf
 	mkdir -p ${3}/${currentenv}
-	screen -L -Logfile ${3}/isaaclog.log -d -m -S ISAACSIM zsh -i -c "cd ${6} && ./python.sh standalone_examples/api/omni.isaac.ros_bridge/my_robot.py --/renderer/enabled='rtx,iray'  --config='/home/ebonetto/.local/share/ov/pkg/isaac_sim-2021.2.1/standalone_examples/api/omni.isaac.ros_bridge/config.yaml' --fix_env=${currentenv} && ./kill.sh"
-	screen -L -Logfile ${3}/rosbag.log -d -m -S ROSRECORDER zsh -i -c "rosbag record -a -O ${3}/${currentenv}/${currentenv}.bag --split --size=1024 -b 0"
+	screen -L -Logfile ${3}/${currentenv}/isaaclog.log -d -m -S ISAACSIM zsh -i -c "cd ${6} && ./python.sh standalone_examples/api/omni.isaac.ros_bridge/my_robot.py --/renderer/enabled='rtx,iray'  --config='/home/ebonetto/.local/share/ov/pkg/isaac_sim-2021.2.1/standalone_examples/api/omni.isaac.ros_bridge/config.yaml' --fix_env=${currentenv} && ./kill.sh"
+	screen -L -Logfile ${3}/${currentenv}/rosbag.log -d -m -S ROSRECORDER zsh -i -c "rosbag record -a -O ${3}/${currentenv}/${currentenv}.bag --split --size=1024 -b 0"
 	sleep 300
 	result=1
+	cnt=3000
 	while [ $result -eq 1 ]
 	do
-		sleep 10
+		sleep 1
 		screen -wipe > suppress_output
 		if ! screen -list | grep -q "ISAACSIM"; then
 			rm suppress_output
 			result=0
 			break
 		fi
+		((cnt=cnt-1))
+		if [[ "$cnt" -eq 0 ]]; then
+			for session in $(screen -ls); do screen -S "${session}" -X quit; done
+			echo "ERROR had to manually stop the sessions" > ${3}/${currentenv}/error.txt
+		fi
 	done
 	#screen -S ROSRECORDER -X stuff "^C"
+	screen -d -m -S ISAACRM zsh -i -c "cd ${6} && rm kit/logs -rf"
 	for session in $(screen -ls | grep -o '[0-9]*\.ROSRECORDER'); do screen -S "${session}" -X stuff "^C"; done
 	screen -d -m -S mover zsh -i -c "mv ${3}/${currentenv} ${1}/"
 	echo "Finished Processing ${currentenv}"
